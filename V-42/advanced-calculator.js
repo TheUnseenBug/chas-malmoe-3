498a09bbdb4c3963abe1c117343473bf5a1abacc
Dennis Granheimer <dennis.granheimer@live.se>
2024-10-18 12:05:59 +0200

    added comments to advanced calc

--- V-42/advanced-calculator.js
+++ V-42/advanced-calculator.js
@@ -1,14 +1,16 @@
+//Visar värdet i display och adderar strängar genom +=
 function appendToDisplay(value) {
   document.getElementById("display").value += value;
 }
+//Sätter värdet i display till en tom sträng
 function clearDisplay() {
   document.getElementById("display").value = "";
 }
-
+//Black magic dont touch
 function calc(value) {
   return new Function("return " + value)();
 }
-
+//Räknar ut svaret med hjälp av calc funktionen
 function calculate() {
   try {
     document.getElementById("display").value = calc(
@@ -17,4 +19,10 @@ function calculate() {
   } catch (error) {
     document.getElementById("display").value = "Error";
   }
+}
+//Tar bort sista värdet i string
+function backspace() {
+  document.getElementById("display").value = document
+    .getElementById("display")
+    .value.slice(0, -1);
 }
