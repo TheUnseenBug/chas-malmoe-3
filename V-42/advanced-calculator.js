300a9b36ce4e72b4e97e8f9093145461059204aa
Dennis Granheimer <dennis.granheimer@live.se>
2024-10-18 11:38:54 +0200

    adv calc changes

--- V-42/advanced-calculator.js
+++ V-42/advanced-calculator.js
@@ -1,20 +1,20 @@
 function appendToDisplay(value) {
-  document.getElementById("display").value += Number(value);
+  document.getElementById("display").value += value;
 }
 function clearDisplay() {
   document.getElementById("display").value = "";
 }
+
+function calc(value) {
+  return new Function("return " + value)();
+}
+
 function calculate() {
-  console.log(typeof document.getElementById("display").value);
   try {
-    document.getElementById("display").value = Number(
+    document.getElementById("display").value = calc(
       document.getElementById("display").value
     );
   } catch (error) {
     document.getElementById("display").value = "Error";
   }
-}
-function backspace() {
-  let currentValue = document.getElementById("display").value;
-  document.getElementById("display").value = currentValue.slice(0, -1);
 }
