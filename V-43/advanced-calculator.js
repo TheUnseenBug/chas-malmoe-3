2c73e618d50be0ddd27b396fdbd75741b2565832
Dennis Granheimer <dennis.granheimer@live.se>
2024-10-25 15:16:09 +0200

    added stuff

--- V-43/advanced-calculator.js
+++ V-43/advanced-calculator.js
@@ -3,41 +3,38 @@ function onOff(value) {
   on = value;
   console.log(on);
 }
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".btn");
+//Visar värdet i display och adderar strängar genom +=
+function appendToDisplay(value) {
+  document.getElementById("display").value += value;
+}
 
-function calculator(appendToDisplay) {
-  //Visar värdet i display och adderar strängar genom +=
-  function appendToDisplay(value) {
-    document.getElementById("display").value += value;
-  }
+//Sätter värdet i display till en tom sträng
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+//calc skapar en ny funktion som har inputs return och kallar den, den nya funktionen har då värdet return + value som nu t.ex. är return 5 + 5.
+function calc(value) {
+  return new Function("return " + value)();
+}
 
-  //Sätter värdet i display till en tom sträng
-  function clearDisplay() {
-    document.getElementById("display").value = "";
-  }
-  //calc skapar en ny funktion som har inputs return och kallar den, den nya funktionen har då värdet return + value som nu t.ex. är return 5 + 5.
-  function calc(value) {
-    return new Function("return " + value)();
-  }
-
-  //Räknar ut svaret med hjälp av calc funktionen
-  function calculate() {
-    try {
-      document.getElementById("display").value = calc(
-        document.getElementById("display").value
-      );
-    } catch (error) {
-      document.getElementById("display").value = "Error";
-    }
-  }
-
-  //Tar bort sista värdet i string
-  function backspace() {
-    document.getElementById("display").value = document
-      .getElementById("display")
-      .value.slice(0, -1);
+//Räknar ut svaret med hjälp av calc funktionen
+function calculate() {
+  try {
+    document.getElementById("display").value = calc(
+      document.getElementById("display").value
+    );
+  } catch (error) {
+    document.getElementById("display").value = "Error";
   }
 }
 
-while (on === true) {
-  calculator();
+//Tar bort sista värdet i string
+function backspace() {
+  document.getElementById("display").value = document
+    .getElementById("display")
+    .value.slice(0, -1);
 }
+
+while (on === true) {}
