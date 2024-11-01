dd285f1db43c0675cf5cbee3370b30ccdb2b66b0
AntonAthley <antonathley@gmail.com>
2024-11-01 12:22:04 +0100

    Update script.js

--- V-44/script.js
+++ V-44/script.js
@@ -1,76 +1,116 @@
 let toDoList = [];
+let toDoApp = true;
+while (toDoApp) {
+  let menu = Number(
+    prompt(
+      "1. Lägg till uppgift. \n2. Visa din lista. \n3. Klarmarkera en uppgift. \n4. Ta bort en uppgift. \n5. Avsluta programmet. "
+    )
+  );
 
-let menu = Number(
-  prompt(
-    "1. Lägg till uppgift. \n2.Visa din lista. \n3. Klarmarkera en uppgift. \n4. Ta bort en uppgift. \n5. Avsluta programmet. "
-  )
-);
-
-switch (menu) {
-  case 1:
-    addTask();
-    break;
-  case 2:
-    showList();
-    break;
-  case 3:
-    completeTask();
-    break;
-  case 4:
-    removeTask();
-    break;
-  case 5:
-    alert("Tack för att du gjort en ToDo-lista :)");
-    break;
-  default:
-    alert("Gör rätt, 1-5");
-}
-
-function addTask() {
-  let taskInput = true;
-  while (taskInput) {
-    let userInput = prompt("Lägg till en ny uppgift:");
-
-    const toDo = { description: userInput, id: toDoList.length, done: false };
-    toDoList.push(toDo);
-    taskInput = confirm("Vill du fortsätta?");
+  switch (menu) {
+    case 1:
+      addTask();
+      break;
+    case 2:
+      showList();
+      break;
+    case 3:
+      completeTask();
+      break;
+    case 4:
+      removeTask();
+      break;
+    case 5:
+      alert("Tack för att du gjort en ToDo-lista :)");
+      toDoApp = false;
+      break;
+    default:
+      alert("Gör rätt, skriv en siffra mellan 1-5");
   }
-}
-// addTask();
 
-// Variabel till showlist för alert ELLER prompt
-function showList() {
-  toDoList.map((item) => alert(`${item.id + 1}. ${item.description}`));
+  function addTask() {
+    let taskInput = true;
+    while (taskInput) {
+      let userInput = String(prompt("Lägg till en ny uppgift:"));
+
+      if (userInput.length < 1) {
+        alert("Uppgiften får inte vara tom, försök igen.");
+      } else {
+        const toDo = {
+          description: userInput,
+          id: toDoList.length,
+          done: false,
+        };
+        toDoList.push(toDo);
+        taskInput = confirm("Vill du fortsätta?");
+      }
+    }
+  }
+
+  // Variabel till showlist för alert ELLER prompt
+  function showList() {
+    alert(
+      toDoList.map((item) => `${item.id}. ${item.description} \n`).join("")
+    );
+    console.log(toDoList);
+  }
+
+  function completeTask() {
+    let taskCompleted = true;
+    while (taskCompleted) {
+      const selectedTask = Number(
+        prompt(
+          `Vilken uppgift är du klar med?
+        ${toDoList
+          .map((item) => `\n ${item.id}. ${item.description}`)
+          .join("")}`
+        )
+      );
+
+      if (selectedTask >= toDoList.length) {
+        alert("Skriv den tillhörande siffran.");
+      } else {
+        toDoList[selectedTask].done = true;
+        alert(`${toDoList[selectedTask].description} är nu klarmarkerade.`);
+        taskCompleted = false;
+        console.log(toDoList);
+      }
+    }
+  }
+
+  //Lös det(loopar även när den arrayen är tom)
+
+  function removeTask() {
+    let taskRemover = true;
+    while (taskRemover) {
+      if (toDoList.length < 1) {
+        alert("Det finns inget att ta bort.");
+        taskRemover = false;
+      } else {
+        const selectedTask = Number(
+          prompt(
+            `Vilken uppgift vill du ta bort?
+        ${toDoList
+          .map((item) => `\n ${item.id}. ${item.description}`)
+          .join("")}`
+          )
+        );
+        if (selectedTask >= toDoList.length) {
+          alert("Skriv den tillhörande siffran.");
+        } else {
+          confirm(
+            `Är du säker på att du vill ta bort ${toDoList[selectedTask].description}?`
+          );
+          if ((confirm = true)) {
+            delete toDoList[selectedTask];
+            // taskRemover = false;
+          } else {
+            taskRemover = false;
+          }
+        }
+      }
+    }
+  }
+
   console.log(toDoList);
 }
-
-// showList();
-
-function completeTask() {
-  const selectedTask = prompt(
-    toDoList.map(
-      (item) =>
-        `Vilken uppgift är du klar med? \n${item.id}. ${item.description}`
-    )
-  );
-  confirm((toDoList[selectedTask].done = true));
-  console.log(toDoList);
-}
-
-// completeTask();
-
-function removeTask() {
-  const selectedTask = prompt(
-    toDoList.map(
-      (item) =>
-        `Vilken uppgift vill du ta bort? \n${item.id}. ${item.description}`
-    )
-  );
-
-  confirm("Är du säker på att du vill ta bort den uppgiften?")
-    ? delete toDoList[selectedTask]
-    : "Du slipper."; //skapa return till menyn
-}
-
-// removeTask();
-console.log(toDoList);
