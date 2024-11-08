445299387f2fa615ac59f8402b9557619e54e45e
Dennis Granheimer <dennis.granheimer@live.se>
2024-11-08 15:48:36 +0100

    broken

--- V-45/script.js
+++ V-45/script.js
@@ -7,12 +7,41 @@ document.getElementById("addBtn").addEventListener("click", (event) => {
   console.log(toDoList);
 });
 
+function toggleModal(title, description, type) {
+  const modalOverlay = document.getElementById("modalOverlay");
+  const modalInput = document.getElementById("modal-input");
+  modalOverlay.style.display = "flex";
+
+  const modalTitle = document.getElementById("modalTitle");
+  modalTitle.textContent = title;
+
+  const modalDescription = document.getElementById("modalDescription");
+  modalDescription.textContent = description;
+
+  closeButton.addEventListener("click", () => {
+    modalOverlay.style.display = "none";
+  });
+
+  if (type !== "edit") {
+    modalInput.style.display = "none";
+  }
+  if (type === "edit") {
+    modalInput.style.display = "block";
+  }
+}
+
+function editTask() {
+  toggleModal("Justera uppgift", "Här kan du byta beskrivning", "edit");
+  // const taskInputElement = document.querySelector(".modal-input");
+  // const userInput = taskInputElement.value;
+}
+
 function addTask() {
   const taskInputElement = document.querySelector(".task-input");
   const userInput = taskInputElement.value;
-
+  console.log(taskInputElement.value);
   if (!userInput) {
-    alert("Du måste skriva någonting, försök igen.");
+    toggleModal("Du måste skriva någonting, försök igen.");
     return;
   }
 
@@ -32,22 +61,26 @@ function displayTask(task) {
   const taskItem = document.createElement("div");
   taskItem.className = "task-item";
 
-  const checkIcon = document.createElement("i");
-  checkIcon.className = "fa-solid fa-square-check check";
-
   const taskDescription = document.createElement("p");
   taskDescription.className = "task";
   taskDescription.textContent = task.description;
 
   const iconsDiv = document.createElement("div");
+  //Kolla över
   iconsDiv.className = "icons";
+
+  const editIcon = document.createElement("i");
+  editIcon.className = "fa-solid fa-edit edit";
+
+  const checkIcon = document.createElement("i");
+  checkIcon.className = "fa-solid fa-square-check check";
 
   const deleteIcon = document.createElement("i");
   deleteIcon.className = "fa-solid fa-trash-can delete";
 
   iconsDiv.appendChild(deleteIcon);
-
-  taskItem.appendChild(checkIcon);
+  iconsDiv.appendChild(checkIcon);
+  iconsDiv.appendChild(editIcon);
   taskItem.appendChild(taskDescription);
   taskItem.appendChild(iconsDiv);
 
@@ -60,4 +93,7 @@ function displayTask(task) {
   });
 
   container.appendChild(taskItem);
+  editIcon.addEventListener("click", () => {
+    editTask();
+  });
 }
