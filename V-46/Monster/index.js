c8873d6ecf3b2394e43e9f17f3debbeb11a5306c
Dennis Granheimer <dennis.granheimer@live.se>
2024-11-14 22:08:42 +0100

    render of monsters

--- V-46/Monster/index.js
+++ V-46/Monster/index.js
@@ -1,17 +1,38 @@
 let monsters = [];
-//Async await så datan hinner hämtas innan den uppdateras
+//Async await så datan hinner hämtas innan den uppdateras sen när datan hämtats rendera den
 async function fetchMonsters() {
   const response = await fetch("monsters.json");
   monsters = await response.json();
   console.log(monsters);
+  renderMonsters();
 }
 fetchMonsters();
 function renderMonsters() {
   const container = document.querySelector(".monster-cards-container");
-  // container.innerHTML = "";
+  container.innerHTML = "";
   monsters.forEach((monster) => {
     const card = document.createElement("div");
     card.className = "monster-card";
+
+    const cardNameContainer = document.createElement("div");
+    cardNameContainer.className = "card-name";
+    const cardName = document.createElement("div");
+    cardName.textContent = monster.name;
+    cardNameContainer.appendChild(cardName);
+
+    const cardSpecialtyContainer = document.createElement("div");
+    cardSpecialtyContainer.className = "card-specialty";
+    const cardSpecialty = document.createElement("div");
+    cardSpecialty.textContent = monster.specialty;
+    cardSpecialtyContainer.appendChild(cardSpecialty);
+
+    const cardImg = document.createElement("img");
+    cardImg.className = "card-img";
+    cardImg.src = monster.image;
+
+    card.appendChild(cardImg);
+    card.appendChild(cardNameContainer);
+    card.appendChild(cardSpecialtyContainer);
+    container.appendChild(card);
   });
 }
-renderMonsters();
