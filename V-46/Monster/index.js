fba26559b26e36c5d4418f32deff999f265482e7
Dennis Granheimer <dennis.granheimer@live.se>
2024-11-14 20:00:29 +0100

    basic style and start js

--- V-46/Monster/index.js
+++ V-46/Monster/index.js
@@ -1,3 +1,17 @@
-fetch("monsters.json")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+let monsters = [];
+//Async await så datan hinner hämtas innan den uppdateras
+async function fetchMonsters() {
+  const response = await fetch("monsters.json");
+  monsters = await response.json();
+  console.log(monsters);
+}
+fetchMonsters();
+function renderMonsters() {
+  const container = document.querySelector(".monster-cards-container");
+  // container.innerHTML = "";
+  monsters.forEach((monster) => {
+    const card = document.createElement("div");
+    card.className = "monster-card";
+  });
+}
+renderMonsters();
