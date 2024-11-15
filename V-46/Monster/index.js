3f25e6de89324c6dfc705d9a7da71506feba37bc
Dennis Granheimer <dennis.granheimer@live.se>
2024-11-15 12:39:28 +0100

    added stuff and changed other things

--- V-46/Monster/index.js
+++ V-46/Monster/index.js
@@ -1,66 +1,77 @@
 let monsters = [];
-let team1 = [];
-let team2 = [];
+const user = {
+  name: "",
+  team: [],
+};
 //Async await så datan hinner hämtas innan den uppdateras sen när datan hämtats rendera den
 async function fetchMonsters() {
   const response = await fetch("monsters.json");
   monsters = await response.json();
-  console.log(monsters);
-  renderMonsters();
+  renderMonsters(".monster-cards-container", monsters, "add");
 }
 fetchMonsters();
-
-//Funktion som renderar modal där title beskrivning ocg typ kan ändras
-function toggleModal(title, description, monster) {
+console.log(user.team);
+//Funktion som renderar modal där title beskrivning och typ kan ändras
+function toggleModal(title, description, monster, type) {
   const modalOverlay = document.getElementById("modalOverlay");
-  const modalInput = document.getElementById("modal-input");
   modalOverlay.style.display = "flex";
 
-  const team1Button = document.getElementById("team1Btn");
-  const team2Button = document.getElementById("team2Btn");
+  const modalContent = document.getElementById("modalContent");
+  const closeButton = document.getElementById("closeButton");
 
-  const modalTitle = document.getElementById("modalTitle");
-  modalTitle.textContent = title;
+  modalContent.innerHTML = "";
+  //FIXME
+  // const modalTitle = document.getElementById("modalTitle");
+  // modalTitle.textContent = title;
+  // const modalDescription = document.getElementById("modalDescription");
+  // modalDescription.textContent = description;
 
-  const modalDescription = document.getElementById("modalDescription");
-  modalDescription.textContent = description;
-
-  closeButton.addEventListener("click", () => {
+  const handleCloseClick = () => {
     modalOverlay.style.display = "none";
-  });
+  };
+  //FIXME
+  // const newCloseButton = closeButton.cloneNode(true);
+  // closeButton.replaceWith(newCloseButton);
+  // closeButton.addEventListener("click", handleCloseClick);
 
-  team1Button.addEventListener("click", () => {
-    addMonsterToTeam(1, monster);
-  });
-  team2Button.addEventListener("click", () => {
-    addMonsterToTeam(2, monster);
-  });
+  if (type === "add") {
+    const teamButton = document.createElement("button");
+    teamButton.textContent = "Add to team";
+    teamButton.id = "teamBtn";
+    modalContent.appendChild(teamButton);
+
+    teamButton.addEventListener("click", () => {
+      addMonsterToTeam(monster);
+      modalOverlay.style.display = "none";
+    });
+  }
+
+  if (type === "remove") {
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove from team";
+    removeButton.id = "removeBtn";
+    modalContent.appendChild(removeButton);
+    removeButton.addEventListener("click", () => {
+      removeMonster(monster);
+      modalOverlay.style.display = "none";
+    });
+  }
 }
 
-function renderMonsters() {
-  const container = document.querySelector(".monster-cards-container");
-  container.innerHTML = "";
-  monsters.forEach((monster) => {
+function renderMonsters(container, render, modalType) {
+  const wrapper = document.querySelector(container);
+  wrapper.innerHTML = "";
+  render.forEach((monster) => {
     const card = document.createElement("div");
     card.className = "monster-card";
 
     const cardDescription = document.createElement("h4");
     cardDescription.textContent = monster.name;
     cardDescription.className = "card-name";
-    // const cardNameContainer = document.createElement("div");
-    // cardNameContainer.appendChild(cardDescription);
-    // const cardName = document.createElement("div");
-    // cardName.textContent = "Name:";
-    // cardNameContainer.appendChild(cardName);
 
     const cardSpecialty = document.createElement("p");
     cardSpecialty.className = "card-specialty";
     cardSpecialty.textContent = monster.specialty;
-    // const cardSpecialtyContainer = document.createElement("div");
-    // cardSpecialtyContainer.appendChild(cardSpecialty);
-    // const cardSpecs = document.createElement("div");
-    // cardSpecs.textContent = "Specialty:";
-    // cardSpecialtyContainer.appendChild(cardSpecs);
 
     const cardImg = document.createElement("img");
     cardImg.className = "card-img";
@@ -69,32 +80,43 @@ function renderMonsters() {
     card.appendChild(cardDescription);
     card.appendChild(cardImg);
     card.appendChild(cardSpecialty);
-    container.appendChild(card);
+    wrapper.appendChild(card);
 
     card.addEventListener("click", () => {
-      toggleModal("Choose a team", "", monster);
+      toggleModal("Choose a team", "", monster, modalType);
     });
   });
 }
-function addMonsterToTeam(team, monster) {
+function addMonsterToTeam(monster) {
   //Glöm inte lägga till localstorage
-  console.log("team:", team, "Monster:", monster);
-  team === 1
-    ? team1.push(monster)
-    : team === 2
-    ? team2.push(monster)
-    : toggleModal("Error", "Team does not exist");
-  console.log(team1);
-  console.log(team2);
-}
-function removeMonster() {
-  //Glöm inte ta bort localstorage
+  console.log(!user.team.includes(monster));
+  if (!user.team.some((member) => member.id === monster.id)) {
+    console.log("first");
+    user.team.push(monster);
+  } else {
+    //FIXME
+    //   toggleModal("Error", "Monster already in team");
+  }
+  renderMonsters("#team-left", user.team, "remove");
 }
 
+function removeMonster(monster) {
+  // Glöm inte ta bort localstorage
+  user.team = user.team.filter((item) => item.id !== monster.id);
+  renderMonsters("#team-left", user.team, "remove");
+  //FIXME
+  //  else {
+  //   toggleModal("Error", "Team does not exist");
+  // }
+}
+
+//FIXME
 function addLocalStorage() {}
-
+//FIXME
 function removeLocalStorage() {}
 
 function createMonster() {}
+//FIXME
+function createUser() {}
 
 function shareGame() {}
