7f7c406935875c17ad92a8f6868d90bfdfd78289
Dennis Granheimer <dennis.granheimer@live.se>
2024-11-22 14:12:33 +0100

    more merge fix

--- V-47/script.js
+++ V-47/script.js
@@ -6,37 +6,36 @@ async function fetchNews(page, category) {
       category ? `&category=${category}` : ""
     }&page=${page}&apiKey=${apiKey}`;
     const response = await fetch(url);
+  } catch (error) {
+    console.error("Error:", error);
   }
-
-
 }
 fetchNews();
 
 function displayNews(news) {
-    const newsFeed = document.getElementById("newsFeed"); // Hämtar newsFeed-elementet
+  const newsFeed = document.getElementById("newsFeed"); // Hämtar newsFeed-elementet
 
-    news.articles.forEach(article => {
-        const newsArticle = document.createElement("article"); // Skapar ett nytt artikel-element
-        newsArticle.classList.add("newsArticle"); // Lägger till klassen "newsArticle"
+  news.articles.forEach((article) => {
+    const newsArticle = document.createElement("article"); // Skapar ett nytt artikel-element
+    newsArticle.classList.add("newsArticle"); // Lägger till klassen "newsArticle"
 
-        // Skapar och lägger till titeln
-        const titleElement = document.createElement("h2");
-        titleElement.textContent = article.title; 
-        newsArticle.appendChild(titleElement); 
+    // Skapar och lägger till titeln
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = article.title;
+    newsArticle.appendChild(titleElement);
 
-        // Skapar och lägger till beskrivningen
-        const descriptionElement = document.createElement("p");
-        descriptionElement.textContent = article.description; // Kort beskrivning
-        newsArticle.appendChild(descriptionElement); 
+    // Skapar och lägger till beskrivningen
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = article.description; // Kort beskrivning
+    newsArticle.appendChild(descriptionElement);
 
+    // Skapar och lägger till källa eller författare
+    const sourceElement = document.createElement("p");
+    sourceElement.textContent = article.source.name; // Källa
+    newsArticle.appendChild(sourceElement);
 
-        // Skapar och lägger till källa eller författare
-        const sourceElement = document.createElement("p");
-        sourceElement.textContent = article.source.name; // Källa
-        newsArticle.appendChild(sourceElement); 
-
-        newsFeed.appendChild(newsArticle); // Lägger till artikeln i newsFeed
-    });
+    newsFeed.appendChild(newsArticle); // Lägger till artikeln i newsFeed
+  });
 }
 
 function filterNews() {
