a72a0b3a2ac2f75a77c531521061c6a64c415a40
Dennis Granheimer <dennis.granheimer@live.se>
2024-11-22 14:37:30 +0100

    add messy stuff

--- V-47/script.js
+++ V-47/script.js
@@ -1,20 +1,22 @@
-let news = [];
+let favoriteNews = [];
 async function fetchNews(page, category) {
   try {
     const apiKey = "db6c1d2353eb42528700f136fd8899fb";
-    const url = `https://newsapi.org/v2/top-headlines?country=us${
-      category ? `&category=${category}` : ""
-    }&page=${page}&apiKey=${apiKey}`;
-    const response = await fetch(url);
+    // const url = `https://newsapi.org/v2/top-headlines?country=us${
+    //   category ? `&category=${category}` : ""
+    // }&page=${page}&apiKey=${apiKey}`;
+    const url = "test.json";
+    const data = await fetch(url);
+    const response = await data.json();
+    console.log(response);
+    displayNews(response);
   } catch (error) {
     console.error("Error:", error);
   }
 }
 fetchNews();
-
 function displayNews(news) {
   const newsFeed = document.getElementById("newsFeed"); // Hämtar newsFeed-elementet
-
   news.articles.forEach((article) => {
     const newsArticle = document.createElement("article"); // Skapar ett nytt artikel-element
     newsArticle.classList.add("newsArticle"); // Lägger till klassen "newsArticle"
@@ -34,18 +36,32 @@ function displayNews(news) {
     sourceElement.textContent = article.source.name; // Källa
     newsArticle.appendChild(sourceElement);
 
+    // Skapar och lägger till källa eller författare
+    const favorite = document.createElement("button");
+    favorite.textContent = "älskar"; // Källa
+    newsArticle.appendChild(favorite);
+
     newsFeed.appendChild(newsArticle); // Lägger till artikeln i newsFeed
+
+    favorite.addEventListener("click", () => {
+      handleFavorite(article);
+    });
   });
 }
-
 function filterNews() {
   //???????
 }
 
 function categoryNews() {}
 
-function favoriteNews() {}
-
+function handleFavorite(article) {
+  if (favoriteNews.includes(article)) {
+    favoriteNews.filter((a) => a.id !== article.id);
+  } else {
+    favoriteNews.push(article);
+  }
+}
+console.log(favoriteNews);
 function searchNews() {}
 
 function pagination() {}
