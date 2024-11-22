2b1ba6767d5bd8b057ce8a27777efeb11b1b27ac
alicegmn <alice.eriksson.561@gmail.com>
2024-11-22 14:27:53 +0100

    Added classes for article elements

--- V-47/script.js
+++ V-47/script.js
@@ -24,20 +24,37 @@ function displayNews(news) {
         newsArticle.classList.add("newsArticle"); // Lägger till klassen "newsArticle"
 
         // Skapar och lägger till titeln
-        const titleElement = document.createElement("h2");
-        titleElement.textContent = article.title; 
-        newsArticle.appendChild(titleElement); 
+        const titleElement = document.createElement("h3");
+        titleElement.textContent = article.title;
+        titleElement.classList.add("newsTitle");
+        newsArticle.appendChild(titleElement);
+       
 
         // Skapar och lägger till beskrivningen
         const descriptionElement = document.createElement("p");
         descriptionElement.textContent = article.description; // Kort beskrivning
-        newsArticle.appendChild(descriptionElement); 
+        descriptionElement.classList.add("newsDescription");
+        newsArticle.appendChild(descriptionElement);
 
 
-        // Skapar och lägger till källa eller författare
+        // Skapar och lägger till författare
         const sourceElement = document.createElement("p");
-        sourceElement.textContent = article.source.name; // Källa
+        sourceElement.textContent = "Published on: " + article.source.name; // Källa
+        sourceElement.classList.add("newsSource");
         newsArticle.appendChild(sourceElement); 
+
+        const authorElement = document.createElement("p");
+        authorElement.textContent = "Written by: " + article.author; // Källa
+        authorElement.classList.add("newsAuthor");
+        newsArticle.appendChild(authorElement); 
+
+        // Skapar och lägger till read full story med länk till nyhetens url
+        const readMore = document.createElement("a");
+        readMore.href = article.url; // Korrekt länk till nyheten
+        readMore.target = "_blank"; // Öppnar länken i en ny flik
+        readMore.textContent = "Read full story"; // Text för länken
+        readMore.classList.add("readMore");
+        newsArticle.appendChild(readMore); // Lägger till länken i artikeln
 
         newsFeed.appendChild(newsArticle); // Lägger till artikeln i newsFeed
     });
