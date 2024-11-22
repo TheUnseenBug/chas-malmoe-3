5d867283b4a460474599470e5eb237b9813cef3c
Dennis Granheimer <dennis.granheimer@live.se>
2024-11-22 14:09:37 +0100

    added pagination system

--- V-47/script.js
+++ V-47/script.js
@@ -1,15 +1,17 @@
-//   "https://newsapi.org/v2/top-headlines?country=us&apiKey=db6c1d2353eb42528700f136fd8899fb"
-
-async function fetchNews() {
+async function fetchNews(page, category) {
   try {
-    const response = await fetch("test.json");
+    const apiKey = "db6c1d2353eb42528700f136fd8899fb";
+    const url = `https://newsapi.org/v2/top-headlines?country=us${
+      category ? `&category=${category}` : ""
+    }&page=${page}&apiKey=${apiKey}`;
+    const response = await fetch(url);
     const data = await response.json();
     console.log(data);
   } catch (error) {
     console.error("Error:", error);
   }
 }
-fetchNews();
+fetchNews(2);
 
 function displayNews() {
   //array.forEach()
