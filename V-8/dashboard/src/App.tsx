2f235acbaabecf9ca5b73d8a0a7b0652261f971c
alicegmn <alice.eriksson.561@gmail.com>
2025-02-21 10:49:36 +0100

    Green button...

--- V-8/dashboard/src/App.tsx
+++ V-8/dashboard/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
   return (
     <>
       <div className="bg-red-500">hello</div>
+      <Button text="Jag är en knapp" />
     </>
   );
 }
---
+++ V-8/dashboard/src/components/Button.tsx
@@ -0,0 +1,9 @@
+function Button({ text }) {
+  return (
+    <button className="rounded-xl bg-green-500 text-white p-5 mt-3 mb-8 font-medium text-2xl">
+      {text}
+    </button>
+  );
+}
+
+export default Button;
