e2d46ea9b39eb5223a2becd888f74f237f592487
AntonAthley <antonathley@gmail.com>
2025-02-21 10:59:56 +0100

    update app.tsx

--- V-8/dashboard/src/App.tsx
+++ V-8/dashboard/src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import Button from "./components/Button";
 import CardOne from "./components/CardOne";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <h1 className="bg-red-500">hello</h1>
       <CardOne />
+      <Footer />
     </>
   );
 }
