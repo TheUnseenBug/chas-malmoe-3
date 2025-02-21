c764f7255ebc6c5fe83dfa8866cf910fb4b041a3
alicegmn <alice.eriksson.561@gmail.com>
2025-02-21 11:37:44 +0100

    app

--- V-8/dashboard/src/App.tsx
+++ V-8/dashboard/src/App.tsx
@@ -2,8 +2,8 @@ import "./App.css";
 import CardOne from "./components/CardOne";
 import Footer from "./components/Footer";
 import Page from "./components/Pagination";
-
 import Header from "./components/Header";
+
 function App() {
   return (
     <>
