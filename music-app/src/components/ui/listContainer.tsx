d17d3c6422f657e55d552110c020b346c25d8b78
Rhibro <rhibron@gmail.com>
2025-02-28 11:49:01 +0100

    just started

---
+++ music-app/src/components/ui/listContainer.tsx
@@ -0,0 +1,9 @@
+export default function ListContainer() {
+    return (
+        <div className="rounded-md border-4 border-black">
+            <p>List of Songs</p>
+        </div>  
+    )
+}
+
+// #EEBB36
--- music-app/src/pages/Home.tsx
+++ music-app/src/pages/Home.tsx
@@ -1,7 +1,10 @@
+import ListContainer from "@/components/ui/listContainer";
+
 export default function Home() {
   return (
     <main>
       <p>Hello and welcome to spotify 2.1</p>
+      <ListContainer />
     </main>
   );
 }
