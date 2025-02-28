c4fba5330507358ae40233d78377ea3643f5e861
alicegmn <alice.eriksson.561@gmail.com>
2025-02-28 11:55:42 +0100

    adding logo

--- music-app/src/components/layouts/Header.tsx
+++ music-app/src/components/layouts/Header.tsx
@@ -1,3 +1,9 @@
 export default function Header() {
-  return <h1>daara.</h1>;
+  return (
+    <header className="p-2">
+      <h1 className="text-white text-left text-5xl">
+        daara<span className="text-pink-500">.</span>
+      </h1>
+    </header>
+  );
 }
