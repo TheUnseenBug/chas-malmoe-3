35c6f5f8acd5ad09c09e5f4f9ce917093c5e0cfe
alicegmn <alice.eriksson.561@gmail.com>
2025-03-02 19:34:39 +0100

    Adding header section

--- music-app/src/components/layouts/Header.tsx
+++ music-app/src/components/layouts/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="p-2">
+    <header className="p-3">
       <h1 className="text-white text-left text-5xl">
         daara<span className="text-pink-500">.</span>
       </h1>
