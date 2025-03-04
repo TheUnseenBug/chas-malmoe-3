1424693ec42b86d72e2c28d51c766d5c41f01a15
dennis granheimer <dennis.granheimer@live.se>
2025-03-04 15:07:51 +0100

    api fix

--- music-app/frontend/src/helpers/useAuth.js
+++ music-app/frontend/src/helpers/useAuth.js
@@ -16,30 +16,30 @@ export default function useAuth(code) {
         setRefreshToken(res.data.refreshToken);
         setExpiresIn(res.data.expiresIn);
         window.history.pushState({}, null, "/");
-      })
-      .catch(() => {
-        window.location = "/";
       });
+    // .catch(() => {
+    //   window.location = "/";
+    // });
   }, [code]);
 
-  useEffect(() => {
-    if (!refreshToken || !expiresIn) return;
-    const interval = setInterval(() => {
-      axios
-        .post("http://localhost:3001/refresh", {
-          refreshToken,
-        })
-        .then((res) => {
-          setAccessToken(res.data.accessToken);
-          setExpiresIn(res.data.expiresIn);
-        })
-        .catch(() => {
-          window.location = "/";
-        });
-    }, (expiresIn - 60) * 1000);
+  // useEffect(() => {
+  //   if (!refreshToken || !expiresIn) return;
+  //   const interval = setInterval(() => {
+  //     axios
+  //       .post("http://localhost:3001/refresh", {
+  //         refreshToken,
+  //       })
+  //       .then((res) => {
+  //         setAccessToken(res.data.accessToken);
+  //         setExpiresIn(res.data.expiresIn);
+  //       })
+  //       .catch(() => {
+  //         window.location = "/";
+  //       });
+  //   }, (expiresIn - 60) * 1000);
 
-    return () => clearInterval(interval);
-  }, [refreshToken, expiresIn]);
+  //   return () => clearInterval(interval);
+  // }, [refreshToken, expiresIn]);
 
   return accessToken;
 }
