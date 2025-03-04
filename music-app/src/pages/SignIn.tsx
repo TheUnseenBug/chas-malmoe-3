c9be4c98cc7186f200f2ee0a0c70f5d0fbc84a13
AntonAthley <antonathley@gmail.com>
2025-03-04 09:02:43 +0100

    social buttons addded

--- music-app/src/pages/SignIn.tsx
+++ music-app/src/pages/SignIn.tsx
@@ -1,14 +1,16 @@
 import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
+import SocialIcons from "@/components/SocialButtons";
 
 export default function SignIn() {
   return (
     <main>
-      <div className="bg-white grid-cols-8">
+      <div className="bg-white max-w-screen-lg flex-row justify-center">
         <h2>Sign in with your Spotify account</h2>
         <Input placeholder="E-mail or username..."></Input>
         <Input placeholder="Password..." type="password"></Input>
         <Button>Sign in</Button>
+        <SocialIcons />
         <p>
           You need a Spotify account and subscription to proceed. Get it{" "}
           <a href="https://www.spotify.com/se/signup" target="_blank">
