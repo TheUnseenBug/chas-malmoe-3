1180b01121bb525528b200e46098d83cfe587ac2
Dennis Granheimer <dennis.granheimer@live.se>
2025-03-07 11:48:13 +0100

    general cleanup

--- music-app/frontend/src/store/store.ts
+++ music-app/frontend/src/store/store.ts
@@ -3,13 +3,20 @@ import { create } from "zustand";
 interface AccessState {
   accessToken: string;
   addAccessToken: (key: string) => void;
+  code: string;
+  addCode: (key: string) => void;
 }
 
 const useAccessStore = create<AccessState>((set) => ({
   accessToken: "",
+  code: "",
   addAccessToken: (token: string) =>
     set(() => ({
       accessToken: token,
+    })),
+  addCode: (code: string) =>
+    set(() => ({
+      code: code,
     })),
 }));
 
