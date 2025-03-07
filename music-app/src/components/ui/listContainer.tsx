0e0949db235db2bf3e1a05f28915004304996770
alicegmn <alice.eriksson.561@gmail.com>
2025-03-07 09:23:21 +0100

    Main to alicegmn

--- music-app/src/components/ui/listContainer.tsx
+++ music-app/src/components/ui/listContainer.tsx
@@ -1,7 +1,7 @@
 export default function ListContainer() {
     return (
         <div className="rounded-md border-4 border-black bg-colors-customYellow m-5">
-            <p>List of Songs</p>
+            <p className="text-2xl">List of Songs</p>
         </div>  
     )
 }
