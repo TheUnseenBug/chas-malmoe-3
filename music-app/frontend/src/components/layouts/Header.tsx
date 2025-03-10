9f1480d1f9ed52ff49a14ba0b23d5eb1f2a4948d
AntonAthley <antonathley@gmail.com>
2025-03-10 10:32:43 +0100

    hover over the dot aswell

--- music-app/frontend/src/components/layouts/Header.tsx
+++ music-app/frontend/src/components/layouts/Header.tsx
@@ -6,11 +6,11 @@ export default function Header() {
       <h1 className="text-white text-left text-5xl">
         <NavLink
           to="/"
-          className="cursor-pointer hover:text-gray-300 transition-colors"
+          className="cursor-pointer hover:opacity-80 transition-opacity duration-200 ease-in-out"
         >
           daara
+          <span className="text-pink-500">.</span>
         </NavLink>
-        <span className="text-pink-500">.</span>
       </h1>
     </header>
   );
