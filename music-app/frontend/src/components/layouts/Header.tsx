ed0de92f81ecb32b9df4deab06ed8d7ba8560960
alicegmn <alice.eriksson.561@gmail.com>
2025-03-10 13:05:12 +0100

    accepted incoming header

--- music-app/frontend/src/components/layouts/Header.tsx
+++ music-app/frontend/src/components/layouts/Header.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
-export default function Header() {
-  return (
-    <header className="p-5">
-      <Link to="/">
-        <h1 className="text-white text-left text-5xl">
-          daara<span className="text-pink-500">.</span>
-        </h1>
-      </Link>
-=======
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
@@ -23,7 +12,6 @@ export default function Header() {
           <span className="text-pink-500">.</span>
         </NavLink>
       </h1>
->>>>>>> 9f1480d1f9ed52ff49a14ba0b23d5eb1f2a4948d
     </header>
   );
 }
