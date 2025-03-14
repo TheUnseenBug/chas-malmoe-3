cb07852bf402ab4098a33a928f9632155fe9541a
alicegmn <alice.eriksson.561@gmail.com>
2025-03-14 11:39:42 +0100

    login page styling

--- music-app/frontend/src/pages/Login.tsx
+++ music-app/frontend/src/pages/Login.tsx
@@ -4,19 +4,23 @@ const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${clientId}&r
 export default function Login() {
   return (
     <div
-      className="flex justify-center items-center"
+      className="flex flex-col justify-center items-center gap-8"
       style={{ minHeight: "100vh" }}
     >
-      <h1 className="text-white text-left text-5xl">
-        dara
-        <span className="text-pink-500">.</span>
-      </h1>
-      <button
-        className="border-black border-4 text-green px-8 py-4 bg-colors-customGreen hover:bg-colors-customBlue hover:text-white rounded-md text-3xl font-semibold"
-        onClick={() => window.location.assign(AUTH_URL)}
-      >
-        Log in with Spotify
-      </button>
+      <div>
+        <h1 className="text-white text-center text-8xl">
+          dara
+          <span className="text-pink-500">.</span>
+        </h1>
+      </div>
+      <div>
+        <button
+          className="border-black border-4 text-green px-8 py-4 bg-colors-customGreen hover:bg-colors-customBlue hover:text-white rounded-md text-3xl font-semibold"
+          onClick={() => window.location.assign(AUTH_URL)}
+        >
+          Log in with Spotify
+        </button>
+      </div>
     </div>
   );
 }
